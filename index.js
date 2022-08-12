b54bc148b4482e876c540a349e865f9ffe6fad44
Ernesto Gilberto Valenzuela Gallardo <ernestogilberto@hotmail.com>
2022-08-11 20:25:24 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Contenedor = require('./contenedor.js');
+
+let products = [
+  {
+    title: 'Escuadra',
+    price: 123.45,
+    thumbnail: 'pendiente'
+  },
+  {
+    title: 'Calculadora',
+    price: 234.56,
+    thumbnail: 'pendiente'
+  },
+  {
+    title: 'Globo Terráqueo',
+    price: 345.67,
+    thumbnail: 'pendiente'
+  }
+]
+
+let productManager = new Contenedor()
